3a5edd73d4d21a6548594e3d2fa3627e49c21ca5
akira muto <akira.muto1124@gmail.com>
2025-02-15 21:18:28 +0900

    8th commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://tech0-gen8-step4-pos-app-10.azurewebsites.net/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://tech0-gen8-step4-pos-app-10.azurewebsites.net";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
